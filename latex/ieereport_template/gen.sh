cf795568166258ebff94d55ded6a6e7846b7227d
byteofsoren <r00tr4t@gmail.com>
2022-03-02 10:19:06 +0100

    added old commmits
    Signed-off-by: byteofsoren <r00tr4t@gmail.com>

--- latex/ieereport_template/gen.sh
+++ latex/ieereport_template/gen.sh
@@ -5,9 +5,9 @@
 
 # Base file is the starting point for the documens with out tex ending i.e main.tex is just main.
 # pdfshow is the pdf viewer.
-baseFile=main
+baseFile="oponent_msn15018"
 pdfshow=zathura
-output_directory=/tmp/pdflatex/
+output_directory=/tmp/pdflatex
 
 
 check_process() {
@@ -21,21 +21,24 @@ then
     mkdir $output_directory
 fi
 #ps aux | grep $pdfshow > /dev/null
-check_process "$pdfshow"
-if [ ! $? -eq 0 ];
-then
-    echo "$pdfshow is running. Kill ing it."
-    pkill $pdfshow
-fi
+# check_process "$pdfshow"
+# if [ ! $? -eq 0 ];
+# then
+#     echo "$pdfshow is running. Kill ing it."
+#     pkill $pdfshow
+# fi
 cp $baseFile.tex $baseFile.bac
 if [ -f $baseFile.tex ];
 then
-    pdflatex -interaction=nonstopmode -output-directory $output_directory $baseFile.tex > /dev/null
-    cp "$output_directory$baseFile.pdf" .
+    echo "Compiling"
+    # pdflatex -interaction=nonstopmode -output-directory $output_directory $baseFile.tex > "$output_directory/$baseFile.log"
+    pdflatex -interaction=nonstopmode -output-directory $output_directory $baseFile.tex
+    # cat $output/$baseFile.log
+    cp "$output_directory/$baseFile.pdf" .
+    echo "Done compiling"
 fi
 if [ -f $baseFile.pdf ];
 then
     $pdfshow $baseFile.pdf &
 fi
-cat $baseFile.log
 #rm *.out *.bac *.bac *.aux > /dev/null
